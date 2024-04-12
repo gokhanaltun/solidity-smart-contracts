73a282f6e32a5b1f344dd0ef152b6b51533d0bf9
Gökhan Altun <86836411+gokhanaltun@users.noreply.github.com>
2024-04-12 20:29:06 +0300

    code descriptions added

--- contracts/token/ERC1155/CoreERC1155.sol
+++ contracts/token/ERC1155/CoreERC1155.sol
@@ -3,10 +3,20 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 
+/**
+ * @title CoreERC1155
+ * @dev Core ERC1155 contract providing basic functionality for an ERC1155 token.
+ */
 abstract contract CoreERC1155 is ERC1155 {
     string private _name;
     string private _symbol;
 
+    /**
+     * @dev Constructor function to initialize the CoreERC1155 contract with the specified name, symbol, and URI.
+     * @param name_ The name of the ERC1155 token.
+     * @param symbol_ The symbol of the ERC1155 token.
+     * @param uri_ The base URI for token metadata. Example: ipfs://CID/
+     */
     constructor(
         string memory name_,
         string memory symbol_,
@@ -16,10 +26,18 @@ abstract contract CoreERC1155 is ERC1155 {
         _symbol = symbol_;
     }
 
+    /**
+     * @dev Returns the name of the ERC1155 token.
+     * @return The name of the ERC1155 token.
+     */
     function name() public view virtual returns (string memory) {
         return _name;
     }
 
+    /**
+     * @dev Returns the symbol of the ERC1155 token.
+     * @return The symbol of the ERC1155 token.
+     */
     function symbol() public view virtual returns (string memory) {
         return _symbol;
     }
--- contracts/token/ERC1155/SimpleOwnerMintERC1155.sol
+++ contracts/token/ERC1155/SimpleOwnerMintERC1155.sol
@@ -1,3 +1,9 @@
+/**
+ * WARNING: This contract is currently in development and all tests have not yet been completed.
+ * Therefore, it is not recommended to use it in production environments.
+ */
+
+
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
@@ -5,6 +11,10 @@ import "./CoreERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
+/**
+ * @title SimpleOwnerMintERC1155
+ * @dev ERC1155 token contract that allows only the owner to mint new tokens.
+ */
 contract SimpleOwnerMintERC1155 is CoreERC1155, Ownable {
     using Strings for uint256;
 
@@ -20,10 +30,20 @@ contract SimpleOwnerMintERC1155 is CoreERC1155, Ownable {
         string memory uri_
     ) CoreERC1155(name_, symbol_, uri_) Ownable(_msgSender()) {}
 
+    /**
+     * @dev Sets the base URI for token metadata.
+     * @param newURI_ The new base URI.
+     */
     function setURI(string memory newURI_) public onlyOwner {
         _setURI(newURI_);
     }
 
+    /**
+     * @dev Mints a new token to the specified account.
+     * @param account_ The address to which the new tokens will be minted.
+     * @param id_ The ID of the token to mint.
+     * @param amount_ The amount of tokens to mint.
+     */
     function mint(
         address account_,
         uint256 id_,
@@ -32,6 +52,11 @@ contract SimpleOwnerMintERC1155 is CoreERC1155, Ownable {
         _mint(account_, id_, amount_, "");
     }
 
+    /**
+     * @dev Returns the URI for a given token ID.
+     * @param tokenId_ The ID of the token.
+     * @return The URI for the token metadata.
+     */
     function uri(
         uint256 tokenId_
     ) public view override returns (string memory) {
